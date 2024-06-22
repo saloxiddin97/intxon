52f83cc7cb9e9e0d4bf2d9c5b858b05471c1f2eb
saloxiddin97 <salohiddinshamsidinov0101@gmail.com>
2024-06-22 12:20:43 +0500

    intxon

---
+++ scrip.js
@@ -0,0 +1,35 @@
+//let a=Math.round(Math.random()*10)
+//alert(a)
+//let a = ["mars", "ayti", "skul"];
+//alert(a.length)
+  //  for (let i = 1; i <= 10; i++) {
+    //  console.log(`${i}) ` + words);
+   // }
+    //let a= [ "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ]
+//let b=["saloxidin",]
+//if(a.b){
+    //console.log(b);
+//}
+//let a=prompt(" yoshingizni kriting")
+
+
+
+//if(a>18){
+  //  alert("siz balogat yoshiga yetgansiz")
+    
+//}
+//else  if(a<18){
+  //  alert("siz balogat yoshiga yetmagansiz")
+    
+//}
+//else {
+  //  alert("siz balogat yoshingiz bilan tabriklaymis")
+//}
+//let numbers=[1, 2 ,3, 4, 5, , 7, 8, 9,10 ] 
+//let pustoy=[];
+//for(i = 0; i < numbers.length; i++){
+ //if(numbers[i] % 2 == 0) {
+  // pustoy.push(numbers[i]);
+//}
+//}
+//console.log(pustoy);
